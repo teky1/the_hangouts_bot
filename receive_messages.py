da94d89e4e99fad260a1ef6968f78e82536d078e
joel <joelchem@outlook.com>
2020-09-28 18:40:57 -0400

    added timeout function to silence the bot for certain amount of time, made language only send the full copypasta for certain people and send a shorter one for everyone else. limited the max name size to 16 characters

--- receive_messages.py
+++ receive_messages.py
@@ -13,12 +13,16 @@ TARGET_CONVO_ID = ["UgySatjjT_zwYMjvfcl4AaABAQ", # Smol one with ash
                    "UgxDGV0wLrqmNtJl6AF4AaABAQ", # Strange Children dwell in this place
                    "UgykUAGlU7YTgn7SqDZ4AaABAQ", # Pranking
                    "Ugyh42XnZTkgwWOdSXx4AaABAQ", # spicy food
+                   "Ugxbq8_Rirg4LzaJIwt4AaABAQ", # Among us thing with 8th graders
                    ]
 
 bullied_people = [
     "1", #nobody
 ]
 
+AUTHORIZED_ADMINS = [
+    "107091478556168807541", #Joel
+]
 
 def getName(user_id_obj):
     name = name_manager.getName(user_id_obj.chat_id)
@@ -26,6 +30,16 @@ def getName(user_id_obj):
         return user_list.get_user(user_id_obj).full_name
     else:
         return name
+
+async def timeout(conv_id_str, length):
+    send_message(conv_id_str, f'Started timeout for {int(length/60)} minutes.')
+    for i in range(len(TARGET_CONVO_ID)):
+        if TARGET_CONVO_ID[i] == conv_id_str:
+            index = i
+    TARGET_CONVO_ID[index] = ""
+    await asyncio.sleep(length)
+    TARGET_CONVO_ID[index] = conv_id_str
+    send_message(conv_id_str, 'Timeout ended. Be good boys and girls now!')
 
 async def receive_messages(client, args):
     print('loading conversation list...')
@@ -41,7 +55,7 @@ async def receive_messages(client, args):
         await asyncio.sleep(1)
 
 
-def on_event(conv_event):
+async def on_event(conv_event):
     if isinstance(conv_event, hangups.ChatMessageEvent):
         if conv_event.conversation_id in TARGET_CONVO_ID:
             print(user_list.get_user(conv_event.user_id).full_name+':', conv_event.text)
@@ -95,9 +109,10 @@ def on_event(conv_event):
                 send_message(return_convo_id, f'You said: {txt}')
 
             elif cmdtxt == "language":
-
-                send_message(return_convo_id, "Hello you fucking bot. My fucking name is fucking ShitPissCum1312 and I am a fucking bot fucking made by some fucking mother-fuckung-fucker who was really fucking annoyed by your fucking comments with a fucking purpose of fucking telling you to fucking shut the fuck up. What the fucking fuck are you even fucking trying to fucking achieve by fucking doing this fucking shit fucking over and over? No fucking one is fucking going to fucking stop fucking saying fucking fuck just because you fucking told them so. Fuck you all and have a nice fucking day. Fuck.")
-
+                if conv_event.user_id.chat_id == "109838171166874380185":
+                    send_message(return_convo_id, "Hello you fucking bot. My fucking name is fucking ShitPissCum1312 and I am a fucking bot fucking made by some fucking mother-fuckung-fucker who was really fucking annoyed by your fucking comments with a fucking purpose of fucking telling you to fucking shut the fuck up. What the fucking fuck are you even fucking trying to fucking achieve by fucking doing this fucking shit fucking over and over? No fucking one is fucking going to fucking stop fucking saying fucking fuck just because you fucking told them so. Fuck you all and have a nice fucking day. Fuck.")
+                else:
+                    send_message(return_convo_id, "Oh shit, your dumb bitch ass saying that makes me want to stop fucking cursing!!!")
             elif cmdtxt == "!time":
 
                 the_time = conv_event.timestamp - timedelta(hours=4)
@@ -140,12 +155,27 @@ def on_event(conv_event):
                     send_message(return_convo_id, "Correct format is !kmk <person1> <person2> <person3>")
 
             elif cmdtxt.startswith('!callme'):
-                if cmdtxt.count(' ')>=1 and len(cmdtxt)>8:
+                if cmdtxt.count(' ')>=1 and len(cmdtxt)>8 and len(cmdtxt)<=24:
                     name = conv_event.text[8:]
                     user_id = conv_event.user_id.chat_id
                     send_message(return_convo_id, f"Set your name to {name_manager.setName(user_id, name)}" )
+                elif len(cmdtxt)>24:
+                    send_message(return_convo_id, f"That name is too long. Names are limited to 16 characters.")
                 else:
                     send_message(return_convo_id, f"Correct Format: !callme <name>.")
+
+            elif cmdtxt.startswith('!timeout'):
+                if conv_event.user_id.chat_id in AUTHORIZED_ADMINS:
+                    try:
+                        amt = int(cmdtxt.replace('!timeout ', ''))
+                    except ValueError:
+                        send_message(return_convo_id, "Correct Format: !timeout <minutes>")
+                        return 0
+                    loop = asyncio.get_event_loop()
+                    loop.create_task(timeout(return_convo_id, amt*60))
+                else:
+                    send_message(return_convo_id, "You are not authorized to use this command.")
+
 
 if __name__ == '__main__':
     run_example(receive_messages)
